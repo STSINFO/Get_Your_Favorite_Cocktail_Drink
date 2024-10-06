4ae27ad240cf87b737400fa7b0c2ea1bd7b9fa8b
STSINFO <sts.info.io@gmail.com>
2024-10-06 12:51:58 +0300

    "Adding some styles and fixing some bugs"

--- js/main.js
+++ js/main.js
@@ -9,37 +9,47 @@ function getYourDrink() {
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data.drinks);
-      data.drinks.forEach((element) => {
-        // image.src = element.strDrinkThumb;
-        // name.textContent = element.strDrink;
-        // pha.textContent = element.strInstructions;
+      if (data) {
+        // console.log(data.drinks);
+        data.drinks.forEach((element) => {
+          // image.src = element.strDrinkThumb;
+          // name.textContent = element.strDrink;
+          // pha.textContent = element.strInstructions;
 
-        //create new elements
-        let newDiv = document.createElement("div");
-        let newImg = document.createElement("img");
-        let newP = document.createElement("P");
-        let newH2 = document.createElement("h2");
+          //create new elements
+          let newDiv = document.createElement("div");
+          let newImg = document.createElement("img");
+          let newP = document.createElement("P");
+          let newH2 = document.createElement("h2");
 
-        //set content to the newly created elements
-        //create a new img
-        newImg.src = element.strDrinkThumb;
-        newImg.alt = element.strIngredient1;
-        newP = element.strInstructions;
-        newH2 = element.strDrink;
+          //set content to the newly created elements
+          //create a new img
 
-        // add classes to the elements
+          newImg.src = element.strDrinkThumb;
+          newImg.alt = element.strIngredient1;
+          newP = element.strInstructions;
+          newH2 = element.strDrink;
 
-        // newImg.classList.add("grid-item-image");
-        // newP.classList.add("grid-item-text");
-        // newH2.classList.add("grid-item-title");
+          //Neither of these techniques is working WHY?
+          //   newDiv.classList.add("grid-item");
+          //   newImg.src = element.classList.add("grid-item-image");
+          //   newImg.alt = element.strIngredient1;
+          //   newP = element.strInstructions.classList.add("grid-item-text");
+          //   newH2 = element.strDrink.classList.add("grid-item-title");
 
-        //append the element to new div
-        newDiv.append(newH2);
-        newDiv.append(newImg);
-        newDiv.append(newP);
-        container.append(newDiv);
-      });
+          // add classes to the elements
+          //   newDiv ? newDiv.classList.add("grid-item") : null;
+          //   newImg ? newImg.classList.add("grid-item-image") : null;
+          //   newP ? newP.classList.add("grid-item-text") : null;
+          //   newH2 ? newH2.classList.add("grid-item-title") : null;
+
+          //append the element to new div
+          newDiv.append(newH2);
+          newDiv.append(newImg);
+          newDiv.append(newP);
+          container.append(newDiv);
+        });
+      }
     })
     .catch((err) => console.log(err));
 }
